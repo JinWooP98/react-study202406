74581911b0bbb2357edc568fcfb4ac9809c2289b
JinWooP98 <qwdk0406@naver.com>
2024-06-20 09:47:50 +0900

    # Todo List 추가하기

--- src/components/Todo/TodoInput.js
+++ src/components/Todo/TodoInput.js
@@ -1,22 +1,38 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {MdAdd} from "react-icons/md";
 
 import './scss/TodoInput.scss';
 
-const TodoInput = ({addTodoList}) => {
+const TodoInput = ({onAdd}) => {
 
 
     // 입력창 토글링 상태값
     const [open, setOpen] = useState(false);
+    // input의 주소값을 기억하는 변수 생성
+    // useRef는 렌더링에 영향을 주지 X
+    const $textInput = useRef();
 
     const onToggle = () => setOpen(prevOpen => !open)
+
+
+    const submitHandler = e => {
+        e.preventDefault();
+
+        // console.log($textInput.current.value);
+        onAdd($textInput.current.value);
+
+        // form이 제출되면 입력창 비우기
+        $textInput.current.value = '';
+        setOpen(false);
+    }
 
     return (
         <>
             { open &&
                 <div className='form-wrapper'>
-                    <form className='insert-form'>
+                    <form className='insert-form' onSubmit={submitHandler}>
                         <input
+                            ref={$textInput}
                             type='text'
                             placeholder='할 일을 입력 후, 엔터를 누르세요!'
                         />
--- src/components/Todo/TodoTemplate.js
+++ src/components/Todo/TodoTemplate.js
@@ -3,15 +3,41 @@ import './scss/TodoTemplate.scss';
 import TodoHeader from "./TodoHeader";
 import TodoInput from "./TodoInput";
 import TodoMain from "./TodoMain";
+import todoItem from "./TodoItem";
 
+
+const DUMMY_TODOS = [
+    {id: 1, title: '리액트 공부', done: true},
+    {id: 2, title: '점심 먹기', done: false},
+    {id: 3, title: '프로젝트', done: false}
+]
 
 const TodoTemplate = () => {
+
+    const [todoList, setTodoList] = useState(DUMMY_TODOS);
+
+    const makeNewId = () => {
+        if(todoList.length < 1) return 1;
+        return todoList[todoList.length - 1].id + 1;
+    }
+
+    // 데이터 상향식 전달을 위한 함수를 생성 (TodoInput에서 할 일을 끌어올리는 역할)
+    const addTodo = (newTodoTitle) => {
+        const newTodo= {
+            id : makeNewId(),
+            title : newTodoTitle,
+            done : false
+        }
+
+        setTodoList(prevTodoList => [...prevTodoList, newTodo]);
+    };
+
 
     return (
         <div className='TodoTemplate'>
             <TodoHeader/>
-            <TodoMain />
-            <TodoInput />
+            <TodoMain todos={todoList} />
+            <TodoInput onAdd={addTodo}/>
         </div>
     );
 };
