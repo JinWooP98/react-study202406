27055e34a3682cd4c5e1b15dfcddaa8c051805d8
JinWooP98 <qwdk0406@naver.com>
2024-06-20 09:11:40 +0900

    # Todo 조건부 렌더링

--- src/components/Todo/TodoInput.js
+++ src/components/Todo/TodoInput.js
@@ -6,35 +6,27 @@ import './scss/TodoInput.scss';
 const TodoInput = ({addTodoList}) => {
 
 
-    const [isInput, setIsInput] = useState(false);
-    const [userInput, setUserInput] = useState('');
+    // 입력창 토글링 상태값
+    const [open, setOpen] = useState(false);
 
-    const changeUserInput = e => {
-        setUserInput(e.target.value);
-    }
 
-    const TodoInputToggle = () => {
-        isInput ? setIsInput(false) : setIsInput(true);
-    }
 
-    const submitTodo = e => {
-        e.preventDefault();
-        addTodoList(userInput);
-        e.target.firstElementChild.value = '';
-    }
+    const onToggle = () => setOpen(prevOpen => !open)
+
+
 
     const renderTodoInput = (
     <>
+    { open &&
         <div className='form-wrapper'>
-            <form className='insert-form' onSubmit={submitTodo}>
+            <form className='insert-form'>
                 <input
                     type='text'
                     placeholder='할 일을 입력 후, 엔터를 누르세요!'
-                    onChange={changeUserInput}
                 />
             </form>
-        </div>
-        <button onClick={TodoInputToggle} className='insert-btn open'>
+        </div>}
+        <button className={`insert-btn ${open ? 'open' : undefined}`} onClick={onToggle}>
             <MdAdd/>
         </button>
     </>
