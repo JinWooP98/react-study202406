7c6ec9b1a56d0cc764ce7277738fe355d419dc63
JinWooP98 <qwdk0406@naver.com>
2024-06-24 12:41:20 +0900

    # 장바구니에서 애니메이션 효과 추가

--- src/components/Food/Layout/HeaderCartButton.js
+++ src/components/Food/Layout/HeaderCartButton.js
@@ -1,19 +1,39 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import CartIcon from "./CartIcon";
 import styles from "./HeaderCartButton.module.scss";
 import CartContext from "../../../store/cart-context";
 
 const HeaderCartButton = ({onShow}) => {
 
-    const { button, icon, badge } = styles;
 
-    const {totalAmount, cartItems} = useContext(CartContext);
+    // bump 애니메이션을 제어하는 상태변수
+    const [isBump, setIsBump] = useState(false);
+
+    const { button, icon, badge, bump } = styles;
+
+    // 장바구니배열
+    const {cartItems} = useContext(CartContext);
+
+    useEffect(() => {
+        if(cartItems.length === 0) return;
+        setIsBump(true);
+
+        // 애니메이션 실행 후 클래스 삭제
+        const timer = setTimeout(() => setIsBump(false), 300)
+
+        return () => {
+            clearTimeout(timer);
+        }
+
+    }, [cartItems]);
 
     const numberOfCart = cartItems.reduce((accum, current) => {
         console.log('a= ',accum);
         console.log(current);
         return accum + current.amount;
     }, 0);
+
+
 
     // console.log(cartItems);
     // let totalAmount = 0;
@@ -24,7 +44,7 @@ const HeaderCartButton = ({onShow}) => {
 
 
     return (
-        <button className={button} onClick={onShow}>
+        <button className={`${button} ${isBump ? bump : undefined}`} onClick={onShow}>
           <span className={icon}>
             <CartIcon/>
           </span>
