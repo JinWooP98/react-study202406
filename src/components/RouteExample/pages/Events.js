13e5e8801bc6a25e98562d100b668eb477fc088f
JinWooP98 <qwdk0406@naver.com>
2024-07-03 12:01:18 +0900

    # error 처리

--- src/components/RouteExample/pages/Events.js
+++ src/components/RouteExample/pages/Events.js
@@ -8,7 +8,7 @@ const Events = () => {
 
     // loader가 리턴한 데이터 받아오기
     const eventList = useLoaderData();
-
+    console.log(eventList);
     return (
     <>
       <h1>Events Page</h1>
@@ -25,13 +25,22 @@ export const loader = async () => {
     // 이 함수에는 페이지가 열리자마자 해야할 일을 적을 수 있다.
 
     // console.log('loader call!!');
-    const response = await fetch('http://localhost:8282/events');
+    const response = await fetch('http://localhost:8282/events?sort=date');
+
     // const jsonData = await response.json();
+    if(!response.ok) {
+        const errorText = await response.text();
+        throw new Response(
+            JSON.stringify({message: errorText}),
+            {
+              status: response.status
+            },
+            );
+    }
 
     // loader가 리턴한 데이터는 loader를 선언한 컴포넌트와 그 하위 컴포넌트에서
     // 언제든 불러서 사용할 수 있다.
 
     // loader에서 fetch의 결과를 바로 리턴하면 알아서 json을 추출해줌.
-
-    return response;
+    return response; // ok일 경우 events[]
 };
