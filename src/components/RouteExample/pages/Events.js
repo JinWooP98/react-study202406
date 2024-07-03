27705f39ce8bc9b13d60c094d02e5d8de7065f78
JinWooP98 <qwdk0406@naver.com>
2024-07-03 10:46:06 +0900

    # loader 기초 사용법

--- src/components/RouteExample/pages/Events.js
+++ src/components/RouteExample/pages/Events.js
@@ -1,26 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import {useLoaderData} from 'react-router-dom';
 import EventList from "../components/EventList";
 
 
 
 const Events = () => {
 
-  const [eventList, setEventList] = useState([]);
+    // loader가 리턴한 데이터 받아오기
+    const eventList = useLoaderData();
 
-  useEffect(() => {
-
-    (async() => {
-      const response = await fetch('http://localhost:8282/events');
-      const jsonData = await response.json();
-      setEventList(jsonData);
-    })();
-
-  }, [])
-
-  
-
-  return (
+    return (
     <>
       <h1>Events Page</h1>
       <EventList eventList={eventList} />
